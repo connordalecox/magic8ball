5b569a18e8a1a8fd3ae127a27a95ade887f50506
Connor Cox <connordalecox@gmail.com>
2022-09-13 18:48:47 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+function myFunction() {
+    let urQuestion = document.getElementById("typehere").value;
+    if (urQuestion== "") {
+        alert("Please enter a question")
+    } else if (urQuestion== "is connor cool") {
+        alert("*Of course! Congrats on finding the secret question!!* ")
+    } else {
+        getAnswer()
+        document.getElementById("question-asked").innerHTML=urQuestion;
+    }
+    
+} 
+const answers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes definitely.",
+    "You may rely on it.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful.",
+];
+
+function getAnswer(){
+    answers.sort(function(a,b){return 0.5 - Math.random()});
+    document.getElementById("answer").innerHTML=answers[0];
+    document.getElementById("answer").style.fontSize="18px";
+    setTimeout(timeup, 2500);
+
+    function timeup(){
+        // document.getElementById("answer").innerHTML="8";
+        // document.getElementById("answer").style.fontSize="120px";
+        document.getElementById("typehere").value="";
+    }
+
+}
+
+const askMe = document.getElementById("askme");
+
+askMe.addEventListener('click', myFunction)
+
+document.getElementById("typehere").onkeydown = function(e) {
+    if(e.key === "Enter"){
+        myFunction()
+    }
+}
